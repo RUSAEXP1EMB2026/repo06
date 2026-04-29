5b279f6ad0e596f9d176dd3cd617290923be5bbe
koumori-K <kouki.mrt1612gmail.com>
2026-04-29 15:12:32 +0900

    claspの設定ファイルと除外設定(.claspignore)を追加

--- line.js
+++ line.js
@@ -1,40 +1,25 @@
+const channelAccessToken = "LYe+EcEVZU45iXW4cy+ANG3589Makj366+o5v5E3YEQU7UiUBrjRVmfoOBnguEbClRzz9I7nwMJWzWsKqFd/ByWLbz1hiymw88yNcqwPd0cS2aa30eDQ9HeKkhIFMCXSycrLqmzSPoNYrF2cLCoSHgdB04t89/1O/w1cDnyilFU="
+
+const URL_LINE = "https://api.line.me/v2/bot/message/";
+const EP_PUSH = URL_LINE + "push";
+
 const SHEET_NAME = "line_log";
 
-const client = LineBotClient.fromChannelAccessToken(
-    {
-        channelAccessToken: "LYe+EcEVZU45iXW4cy+ANG3589Makj366+o5v5E3YEQU7UiUBrjRVmfoOBnguEbClRzz9I7nwMJWzWsKqFd/ByWLbz1hiymw88yNcqwPd0cS2aa30eDQ9HeKkhIFMCXSycrLqmzSPoNYrF2cLCoSHgdB04t89/1O/w1cDnyilFU="
-    }
-);
+function doPost(e){
+  const sheet = getSheet(SHEET_NAME);
 
-function doPost(e) {
-    let sheet = getSheet(SHEET_NAME);
-    sheet.appendRow([new Date(), e.postData.contents]);
-    JSON.parse(e.postData.contents).events.forEach(event => {
-        let eventType = event.type;
-        switch (eventType) {
-            case "message":
-                // メッセージを受け取った際の処理を記述
-                let messageType = event.message.type;
-                switch(messageType) {
-                    case "text":
-                        // テキストメッセージを受け取った際の処理を記述
-                        sheet.appendRow([new Date(), event.message.from, event.message.text]);
-                        break;
-                }
-                break;
-            case "follow":
-                client.pushMessage(
-                    {
-                        to: event.message.userId,
-                        messages: [{
-                            type: "text",
-                            text: "はじめまして！"
-                        }]
-                    }
-                ).then();
-                break;
-        }
-    })
+  const data = JSON.parse(e.postData.contents);
+  const event = data.events[0];
+
+  let text = "";
+
+  if (event.message && event.message.type === "text") {
+    text = event.message.text;
+  } else {
+    text = "テキスト以外";
+  }
+
+  sheet.appendRow([new Date(), event.message.userId , text]);
 }
 
 function sendTextMessage(userId, message) {
@@ -45,6 +30,28 @@ function sendTextMessage(userId, message) {
             text: message
         }]
     }
+}
 
-    client.pushMessage(request).then();
+function sendTextMessage(userId, message) {
+    const request = {
+        to: userId,
+        messages: [{
+            type: "text",
+            text: message
+        }]
+    }
+    const options = {
+        method: "post",
+        contentType: "application/json",
+        payload: JSON.stringify(request),
+        headers: {
+            "Authorization": "Bearer " + channelAccessToken
+        }
+    }
+
+    UrlFetchApp.fetch(PUSH_URL, options);
+}
+
+function test() {
+  sendTextMessage()
 }
